972e3f488a22f90a2be309ce450016d7d72e9120
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2017-09-12 15:34:46 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=2;
+printf("enter the values");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the positve number)
+}
+elseif(a<0)
+{
+printf("the nagative number");
+}
+else
+{
+printf("zero");
+}
+}
