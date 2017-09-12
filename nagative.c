2982b051355171b56348b845c00a0c6f02145c04
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2017-09-12 15:35:52 +0530

    Rename positive.c to nagative.c

---
+++ nagative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the values");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the positve number)
+}
+elseif(a<0)
+{
+printf("the nagative number");
+}
+else
+{
+printf("zero");
+}
+}
