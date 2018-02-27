f7a7a729b32f945b47256feb5c17d7c95395e8a7
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-02-27 16:44:43 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  float l,b,area;
+  printf("enter the length");
+  scanf("%f",&l);
+  printf("enter the breath");
+  scanf("%f",&b);
+  area=l*b;
+  printf("the area is %d.5f",area);
+  return 0;
+}
