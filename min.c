e6eddd6a5bb1f678e48222396c94fda6ba7c33be
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-02-27 15:50:21 +0530

    Create min.c

---
+++ min.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x,y,z;
+printf("entr the hr:min %d",x);
+scanf("%d",&x);
+printf("enter the hr:min %d",y);
+scanf("%d",&y);
+z=x-y;
+printf("ans is",z);
+return 0;
+}
