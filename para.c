cc9d97c8fb834edd0f5e340166c23393cbaa010c
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-03-01 16:30:34 +0530

    Create para.c

---
+++ para.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char str[30];
+    int count,i;
+    printf("enter the string");
+    scanf("%s",&str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+        {
+        count++;
+        }
+    }
+    printf("the number of paragraph%d",++count);
+    
+}
