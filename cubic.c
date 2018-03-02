d3e12cc50415a62133ea90f4413c2228f114ea6d
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-03-02 14:57:08 +0530

    Create cubic.c

---
+++ cubic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int l,b,h,c;
+    printf("enter the length");
+    scanf("%d",&l);
+    printf("enter the breath");
+    scanf("%d",&b);
+    printf("enter the heiht");
+    scanf("%d",&h);
+    c=l*b*h;
+    printf("the cubiod is%d",c);
+}
