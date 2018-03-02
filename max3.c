f0dfed9debb6ff8a872f4c302aa2a616e516cc55
AnusuyaE <31503436+AnusuyaE@users.noreply.github.com>
2018-03-02 14:11:25 +0530

    Create max3.c

---
+++ max3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],max,n,i,j;
+    for(i=0;i<10;i++)
+{
+    scanf("%d",&a[1]);
+}
+max=a[0];
+for(j=0;j<10;j++)
+{
+    if(a[i]>max)
+    max=a[i];
+}
+printf("the max:%d",max);
+return 0;
+    
+}
